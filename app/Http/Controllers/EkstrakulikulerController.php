8c7ff132f2537ac37a7d51991a614fb8f19ab5d5
Rhafinz <raihansrafi@gmail.com>
2024-05-06 20:46:28 +0700

    push project kedua

---
+++ app/Http/Controllers/EkstrakulikulerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ekstrakulikuler;
+use App\Http\Requests\StoreEkstrakulikulerRequest;
+use App\Http\Requests\UpdateEkstrakulikulerRequest;
+
+class EkstrakulikulerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreEkstrakulikulerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreEkstrakulikulerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ekstrakulikuler  $ekstrakulikuler
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ekstrakulikuler $ekstrakulikuler)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ekstrakulikuler  $ekstrakulikuler
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ekstrakulikuler $ekstrakulikuler)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateEkstrakulikulerRequest  $request
+     * @param  \App\Models\Ekstrakulikuler  $ekstrakulikuler
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateEkstrakulikulerRequest $request, Ekstrakulikuler $ekstrakulikuler)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ekstrakulikuler  $ekstrakulikuler
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ekstrakulikuler $ekstrakulikuler)
+    {
+        //
+    }
+}
